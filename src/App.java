592339ffabef6d3452e1f860148bc6d1b1400844
Bryan-Eka-Santoso <bryanekasantoso@gmail.com>
2025-03-05 19:55:17 +0700

    add display for menu

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int menuAwal;
+
+        Scanner scanI = new Scanner(System.in);
+
+        do {
+            System.out.println("== Fire Boy & Water Girl ==");
+            System.out.println("1. Play");
+            System.out.println("2. Your Progress");
+            System.out.println("3. Exit");
+            do {
+                System.out.print(">> ");
+                menuAwal = scanI.nextInt();
+            } while (menuAwal < 1 || menuAwal > 3);
+
+            if(menuAwal == 1){
+
+            } else if (menuAwal == 2){
+                System.out.println("= Your Progress =");
+            }
+
+        } while (menuAwal !=3);
     }
 }

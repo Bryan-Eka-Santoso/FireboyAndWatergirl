28aa05d164cab8d33e5796210237554bda778e20
Bryan-Eka-Santoso <bryanekasantoso@gmail.com>
2025-03-05 20:55:48 +0700

    add move for fireboy and watergirl

--- src/App.java
+++ src/App.java
@@ -1,14 +1,32 @@
 import java.util.*;
 
 public class App {
+    // For System("Cls");
+    public static void clear() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         int menuAwal, player = 0;
         String keyGame;
         int coin = 0;
-        boolean done = false;
+        boolean win = false;
+        boolean door = false;
 
         Scanner scanI = new Scanner(System.in);
         Scanner scanS = new Scanner(System.in);
+
+        int xb = 2, yb = 9;
+        int xg = 1, yg = 9;
 
         // 1 =
         // 2 |
@@ -25,7 +43,7 @@ public class App {
             {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
             {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
             {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-            {2, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+            {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
             {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
         };
 
@@ -49,6 +67,9 @@ public class App {
 
                     for(int i = 0; i < 11; i++){
                         for(int j = 0; j < 23; j++){
+                            map[yb][xb] = 3; 
+                            map[yg][xg] = 4; 
+
                             if(map[i][j] == 1){
                                 System.out.print("=");
                             } else if(map[i][j] == 2){
@@ -73,37 +94,49 @@ public class App {
                     keyGame = scanS.nextLine();
 
                     if(player % 2 == 0){
-                        if(keyGame.charAt(0) == 'A'){
-                            
-                        } else if (keyGame.charAt(0) == 'D'){
-
+                        if(keyGame.charAt(0) == 'A' && map[yb][xb+1] == 0){
+                            map[yb][xb] = 0;
+                            xb++;
+                            player++;
+                        } else if (keyGame.charAt(0) == 'D' && map[yb][xb-1] == 0){
+                            map[yb][xb] = 0;
+                            xb--;
+                            player++;
                         } else if (keyGame.charAt(0) == 'Q'){
 
                         } else if (keyGame.charAt(0) == 'E'){
 
                         }
                     } else {
-                        if(keyGame.charAt(0) == 'A'){
-
-                        } else if (keyGame.charAt(0) == 'D'){
-
+                        if(keyGame.charAt(0) == 'A' && map[yg][xg+1] == 0){
+                            map[yg][xg] = 0;
+                            xg++;
+                            player++;
+                        } else if (keyGame.charAt(0) == 'D' && map[yg][xg-1] == 0){
+                            map[yg][xg] = 0;
+                            xg--;
+                            player++;
                         } else if (keyGame.charAt(0) == 'Q'){
 
                         } else if (keyGame.charAt(0) == 'E'){
 
                         }
                     }
-
+                    clear();
                 } while (keyGame.charAt(0) != '0');
 
                 if(keyGame.charAt(0) == '0'){
                     System.out.println("Game Over!");
+                } else {
+                    System.out.println("You Win!");
+                    coin = 0;
+                    win = false;
                 }
 
             } else if (menuAwal == 2){
                 System.out.println("= Your Progress =");
                 System.out.println("Coin: " + coin + "/2");
-                if(done){
+                if(win){
                     System.out.println("Exit Reached!");
                 } else {
                     System.out.println("Exit Not Reached Yet!");
